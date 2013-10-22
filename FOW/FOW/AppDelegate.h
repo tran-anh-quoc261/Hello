949f72c3b735be4bb95d2404a7325e6230d20823
Anh Quoc <mulodo1@Anh-Quoc-Mac.local>
2013-10-22 15:13:55 +0700

    Comment

---
+++ FOW/FOW/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FOW
+//
+//  Created by Anh Quoc on 10/22/13.
+//  Copyright (c) 2013 Mulodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FOW/FOW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FOW
+//
+//  Created by Anh Quoc on 10/22/13.
+//  Copyright (c) 2013 Mulodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
